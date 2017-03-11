161cd038233d71cd5e334a21198d3e7a7e03de1a
Osama Imtiaz <osamaimtiaz951@gmail.com>
2017-03-12 00:15:34 +0500

    First File

---
+++ js.js
@@ -0,0 +1,22 @@
+
+
+num = prompt("Table" , "e.g. 5")
+document.write( num + " X 1 = " + num*1)
+document.write( "<br/>" + num + " X 2 = " + num*2)
+document.write( "<br/>" + num + " X 3 = " + num*3)
+document.write( "<br/>" + num + " X 4 = " + num*4)
+document.write( "<br/>" + num + " X 5 = " + num*5)
+document.write( "<br/>" + num + " X 6 = " + num*6)
+document.write( "<br/>" + num + " X 7 = " + num*7)
+document.write( "<br/>" + num + " X 8 = " + num*8)
+document.write( "<br/>" + num + " X 9 = " + num*9)
+document.write( "<br/>" + num + " X 10 = " + num*10)
+
+
+
+
+// var num = 10
+// num =  ++num + num++ + --num + num++ + ++num + --num + 2 + num
+// alert(num)
+
+
